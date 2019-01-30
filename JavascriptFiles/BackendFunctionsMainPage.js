55bdfd767cebc0ca24f15289bb9aad5a16989543
Jordan Hill <33611224+coderguy1777@users.noreply.github.com>
2019-01-30 00:29:00 -0500

    Create BackendFunctionsMainPage.js

---
+++ JavascriptFiles/BackendFunctionsMainPage.js
@@ -0,0 +1,14 @@
+function devupdates() {
+    window.open('DevelopmentUpdates.html', '_self', false);
+}
+function onClick(url) {
+    var win = window.open(url, '_blank');
+}
+
+function animationprojects() {
+    window.open('AnimationProjects.html', '_self', false);
+}
+
+function codeupdates() {
+    window.open('CodingProjectsArea.html', '_self', false);
+}
