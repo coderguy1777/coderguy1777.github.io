f8be4d951eddabee04fa17f0580de8e2682f4548
coderguy1777 <hill.jordan@charterschool.org>
2019-06-03 10:17:40 -0400

    Site revemp

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function openGithub() {
+    window.open('https://www.github.com/coderguy1777');
+}
+
+function openAnimation() {
+    window.open('animation.html', '_self', false);
+}
+
+function openGames() {
+    window.open('games.html', '_self', false);
+}
