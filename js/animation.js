9b91bce624a96fd4a410ca9045fe989c33f27c8f
coderguy1777 <hill.jordan@charterschool.org>
2019-06-15 00:02:33 -0400

    Animations worked on for render display

---
+++ js/animation.js
@@ -0,0 +1,15 @@
+let renders_2019 = document.getElementById("render-year-2019");
+let renders_2018 = document.getElementById("render-year-2018");
+let renders_2017 = document.getElementById("render-year-2017");
+
+function open_renders_2019() {
+    window.open('2019renders.html', '_self', false);
+}
+
+function open_renders_2018() {
+    window.open('2018renders.html', '_self', false);
+}
+
+function open_renders_2017() {
+    window.open('2017renders.html', '_self', false);
+}
